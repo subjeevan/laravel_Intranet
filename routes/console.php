8a7036bb430987c03e4cd9b389cf8d18b671e765
Jeevan Subedi <subjeevan@hotmail.com>
2025-03-06 08:30:29 +0545

    email

--- routes/console.php
+++ routes/console.php
@@ -10,3 +10,13 @@ ScheduleFacade::command('email:send-scheduled')->dailyAt('08:29');
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// MAIL_MAILER=smtp
+// MAIL_SCHEME=null
+// MAIL_HOST=smtp.gmail.com
+// MAIL_PORT=465
+// MAIL_USERNAME=no.reply.beh@gmail.com
+// MAIL_PASSWORD="ekrz mcbl knzx llob"
+// MAIL_FROM_ADDRESS="no.reply.beh@gmail.com"
+// MAIL_FROM_NAME="${APP_NAME}"
